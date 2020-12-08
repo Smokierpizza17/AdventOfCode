a7e2dc2a6c82d4590e75092c739ac79a0ff096a2
Smokierpizza17 <oskar.s.munro@gmail.com>
2020-12-08 18:53:26 +0100

    day 7 part 1 complete

--- handyHaversacks/handyhaversacks.py
+++ handyHaversacks/handyhaversacks.py
@@ -1,54 +1,48 @@
-with open("input2.txt", "r") as inputFile:
+with open("input.txt", "r") as inputFile:
     taskInput = inputFile.read().split("\n")
 
 taskInput.pop(-1)  # removes newline at the end of the file
 
-bags = {}  # key is a bag, def is list of sets with how often it can contain
-# a different bag
+parentFinder = {}  # key is a bag, def is list of sets with how often it can be
+# in a different bag
+childFinder  = {}  # key is a bag, def is list of sets with how often it
+# contain in a different bag
 
 
-def findChains(matrix, startingPoint, path=[]):
-    '''returns all sequences that contain startingPoint at the start'''
-    if path == []:
-        path = [startingPoint]
-    else:
-        path.append(startingPoint)
-
-    if startingPoint not in matrix.keys():
-        print(path)
-        path = []
+def findChains(matrix, startingPoint, bags=[], master=False):
+    '''returns all bags that can contain startingPoint'''
+    if startingPoint not in matrix:
         return
-
-    for container in matrix[startingPoint]:
-        findChains(matrix, container[0], path)
+    for parentBag in matrix[startingPoint]:
+        parentColour = parentBag[0]
+        if parentColour not in bags:
+            bags.append(parentColour)
+            findChains(matrix, parentColour, bags)
+    if master:
+        return bags
 
 
 for rule in taskInput:
     splitRule = rule.split(" contain ")
     parentColour = splitRule[0].split(" bag")[0]
 
-    subBags = []
+    subbags = []
     for subBag in splitRule[1].split(", "):
-        strippedSubBags = subBag.split(" bag")[0]
-        if strippedSubBags == "no other":
+        strippedsubbags = subBag.split(" bag")[0]
+        if strippedsubbags == "no other":
             subColour = None
             subNumber = 0
         else:
-            subNumber = strippedSubBags[0]
-            subColour = strippedSubBags[2:]
-        subBags.append((subColour, subNumber))
+            subNumber = strippedsubbags[0]
+            subColour = strippedsubbags[2:]
+        subbags.append((subColour, subNumber))
 
-    for bag in subBags:
-        if bag[0] in bags:
-            bags[bag[0]].append((parentColour, bag[1]))
+    for bag in subbags:
+        if bag[0] in parentFinder:
+            parentFinder[bag[0]].append((parentColour, bag[1]))
         else:
-            bags[bag[0]] = [(parentColour, bag[1])]
+            parentFinder[bag[0]] = [(parentColour, bag[1])]
+    childFinder[parentColour] = subbags
+    
 
-for bag in bags.keys():
-    print(bag)
-    print(bags[bag])
-
-findChains(bags, "shiny gold")
-
-# magic code, long story
-allPathsWithGold = [list(leaf.path) for leaf in PreOrderIter(f, filter_=lambda node: node.is_leaf)]
+print(len(findChains(parentFinder, "shiny gold", master=True)))
