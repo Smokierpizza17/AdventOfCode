daa39b97aee8e119e242011c25bba29906a3cec3
Smokierpizza17 <oskar.s.munro@gmail.com>
2020-12-10 18:05:15 +0100

    day 10 part 1 complete

---
+++ adapterArray/part2.py
@@ -0,0 +1,30 @@
+with open("input.txt", "r") as inputFile:
+    taskInput = inputFile.read().split("\n")
+
+taskInput.pop(-1)  # removes newLine at the end of the File
+
+intTaskInput = [0]  # 0 to include airplane port
+for i in taskInput:  # make list of int instead of str
+    intTaskInput.append(int(i))
+
+intTaskInput.sort()
+
+intTaskInput.append(intTaskInput[-1] + 3)  # factor in device adapter
+
+oneDiffCounter = 0
+threeDiffCoutner = 0
+for i in range(1, len(intTaskInput)):
+    curr = intTaskInput[i]
+    prev = intTaskInput[i - 1]
+    diff = curr - prev
+
+    if diff == 1:
+        oneDiffCounter += 1
+    elif diff == 3:
+        threeDiffCoutner += 1
+    else:
+        raise TypeError("difference other than one or three found!")
+
+print("one diffs: %s" % oneDiffCounter)
+print("three difs : %s" % threeDiffCoutner)
+print("product: %s" % (oneDiffCounter * threeDiffCoutner))
